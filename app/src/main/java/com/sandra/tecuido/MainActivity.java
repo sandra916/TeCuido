a3045f2f4efd2ad01359cb4c9c4a1c73647b4b9a
ynrd <ynrd93@gmail.com>
2019-12-06 00:16:26 +0100

    Icons menu (Pharmacy and Pills) and a new class for each one

--- app/src/main/java/com/sandra/tecuido/MainActivity.java
+++ app/src/main/java/com/sandra/tecuido/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sandra.tecuido;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 
@@ -12,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
 
     }
+    public void farmacia (View view){
+        Intent pharmacy = new Intent(this,Farmacia.class);
+        startActivity(pharmacy);
+    }
+
+    public void medicacion (View view){
+        Intent medication = new Intent(this, Medicacion.class);
+        startActivity(medication);
+    }
+
     public void salir(View v) {
        finish();
     }
